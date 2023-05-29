668663d89d5857ef9ed8c5b00c5377528baa1ac8
jparraporcar <jordiparraporcar@gmail.com>
2023-05-29 14:10:09 +0900

    managed a deterministic response for each section

--- src/api.ts
+++ src/api.ts
@@ -26,6 +26,7 @@ module.exports.indexCourse = async (
                 content: `Create an index/summary for an online course based on a given name and a degree of difficulty. The degree of difficulty can be easy, normal and hard. The online course must have ${query.numberSections} sections, only the name of the sections must be output, and the content of your answer must be inside a javascript array, and no more content in apart from this javascript array can be output, the name of the array and the type of variable cannot be output, only output what is inside the 2 brackets, including the brackets theirselves, for exampple, if you were to output 'let index = [a,b,c], I want you to only output [a,b,c] and I dont want you to output 'let index'. The course name is ${query.courseName} and the difficulty is ${query.difficulty}`,
             },
         ],
+        temperature: 0,
     })
 
     console.log(completion.data.choices[0].message?.content)
@@ -51,11 +52,52 @@ module.exports.courseSectionQuiz = async (
             },
             {
                 role: 'user',
-                content: `Divide the section with name ${query.sectionName} into ${query.subsectionsNumber} subsections and for each subsection create ${query.numberOfQuestionsPerSubsection} questions with ${query.numberOfPosibleSolutionsPerSubsection} multiple solutions where only one is correct, also tell which is the correct solution. Don't output any information related with other sections, only about the section requested. Pack the content inside a json object so that it can be included in a response body and parsed easily`,
+                content: `Divide the section with name ${query.sectionName} (${query.sectionName} is the section that needs to be split into 2 subsections and this name must be substituted in the property value of "sectionName": ${query.sectionName}) into ${query.subsectionsNumber} subsections and for each subsection create ${query.numberOfQuestionsPerSubsection} questions with ${query.numberOfPosibleSolutionsPerSubsection} multiple solutions where only one is correct, also tell which is the correct solution. Don't place a number at the start of the subsections title. The structure of the output is detailed as follows:
+                {"sectionName": "sample",
+                    "subsection1": {
+                        "subsectionName": "sample",
+                        "questions": [
+                            {
+                                "questionName": "sample",
+                                "a": "sample",
+                                "b": "sample",
+                                "c": "sample",
+                                "correct": "sample"
+                            },
+                            {
+                                "questionName": "sample",
+                                "a": "sample",
+                                "b": "sample",
+                                "c": "sample",
+                                "correct": "sample"
+                            }
+                        ]
+                    },
+                    "subsection2": {
+                        "subsectionName": "sample",
+                        "questions": [
+                            {
+                                "questionName": "sample",
+                                "a": "sample",
+                                "b": "sample",
+                                "c": "sample",
+                                "correct": "sample"
+                            },
+                            {
+                                "questionName": "sample",
+                                "a": "sample",
+                                "b": "sample",
+                                "c": "sample",
+                                "correct": "sample"
+                            }
+                        ]
+                    }
+                }`,
             },
         ],
         temperature: 0,
     })
+    console.log(query)
 
     return {
         statusCode: 200,
