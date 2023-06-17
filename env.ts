32e8635af7761f67b0bd9aa47f087c0c532dbee1
jparraporcar <jordiparraporcar@gmail.com>
2023-06-17 16:32:44 +0900

    fix env issue

--- env.ts
+++
@@ -1,7 +0,0 @@
-export const envVars = {
-    // default values
-    STAGE: 'dev',
-    AWS_DEFAULT_REGION: 'ap-northeast-1',
-    OPENAI_API_KEY: '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',
-    ...process.env,
-}
